4865203ae5ab47b4a7e23fae9f68cedcef8038bb
t-davey <tdavey1995@gmail.com>
2016-11-19 14:03:19 -0500

    Shape creation fixes
    Fixed string comparrisons in buildCardObject and buildColorEnum issues.
    Fixed random number generator syntax issues.

--- The Shape is Right/application/Main.java
+++ The Shape is Right/application/Main.java
@@ -82,7 +82,8 @@ public class Main extends Application {
 			ArrayList<ComboBox> guessInputFields = new ArrayList<ComboBox>();
 			ArrayList<Shape> shapeDisplay = new ArrayList<Shape>();
 			
-			Map shapes = new HashMap();
+			
+			Map<String, Shape> shapes = new HashMap<String, Shape>();
 			shapes.put( "Circle", new Circle() );
 			shapes.put( "Rectangle", new Rectangle() );
 			shapes.put( "Circle", new Circle() );
@@ -133,6 +134,7 @@ public class Main extends Application {
 			controlBox.getChildren().add( selectN );
 			controlBox.getChildren().add( shapeList );
 			controlBox.getChildren().add( colorList );
+			//controlBox.getChildren().addAll( guessInputFields );
 			
 			
 			//Assigning nodes to grid positions
@@ -141,6 +143,12 @@ public class Main extends Application {
 			GridPane.setConstraints( selectN, 1, 0 );
 			GridPane.setConstraints( shapeList, 0, 1 , 1, 2);
 			GridPane.setConstraints( colorList, 2, 1 , 1, 2);
+			
+			for ( int i = 0; i < guessInputFields.size(); i++ ){
+				controlBox.getChildren().add( guessInputFields.get( i ) );
+				GridPane.setConstraints( guessInputFields.get( i ), i, 0);
+				guessInputFields.get( i ).setVisible( false );
+			}
 			
 			
 			//setHalignment and setValignments for controls in the buttonBox GridPane
@@ -158,6 +166,7 @@ public class Main extends Application {
 			scene.getStylesheets().add( getClass().getResource("application.css").toExternalForm() );
 			
 			
+			//EventHandler for shapeList ListView to get shape choices from user.
 			shapeList.getSelectionModel().getSelectedItems().addListener(
 					new ListChangeListener<String>() {
 						public void onChanged
@@ -167,6 +176,7 @@ public class Main extends Application {
 					});
 			
 			
+			//EventHandler for colorList ListView to get color choices from user.
 			colorList.getSelectionModel().getSelectedItems().addListener(
 					new ListChangeListener<String>() {
 						public void onChanged
@@ -176,29 +186,46 @@ public class Main extends Application {
 					});
 			
 			
+			//EventHandler for submitButton Button to gather game setup info from user.
 			submitButton.setOnAction( new EventHandler<ActionEvent>() {
 
 				@Override
 				public void handle(ActionEvent arg0) {
+					
+					System.err.println( "Disabling default controls.");
+					
 					shapeList.setVisible( false );
 					colorList.setVisible( false );				
 					selectN.setVisible( false );				
 					submitButton.setVisible( false );		
 					
+					System.err.println( "Generating random shapes and colors." );
+					
 					shapeRand = new Random( shapeInput.size() );
 					colorRand = new Random( colorInput.size() );
 
+					
                     for(int i = 0; i < N; i++) {
+                    	System.err.println( "Building shapes." );
                         // get a random shape, add it to array
-                        shapeDisplay.add(buildCardObject(shapeInput.get(shapeRand.nextInt())));
+                    	
+                        shapeDisplay.add(buildCardObject(shapeInput.get(shapeRand.nextInt(shapeInput.size()))));
                         // fill that shape with random color
-                        shapeDisplay.get(i).setFill(buildColorEnum(colorInput.get(colorRand.nextInt())));
+                        shapeDisplay.get(i).setFill(buildColorEnum(colorInput.get(colorRand.nextInt(colorInput.size()))));
                         // set a white stroke to make it more visible
                         shapeDisplay.get(i).setStroke(Color.WHITE);
                     }
 					
+                    System.err.println( "Creating guess fields." );
+                    
 					for ( int i = 0; i < N; i++ ) {
 						guessInputFields.add( new ComboBox<String>() );
+					}
+					
+					System.err.println( guessInputFields );
+					
+					for ( int i = 0; i < guessInputFields.size(); i++ ){
+						guessInputFields.get( i ).setVisible( true );
 					}
 														
 				}
@@ -206,6 +233,7 @@ public class Main extends Application {
 			});
 
 
+			//EventHandler for selectN ComboBox to get N value from user.
 			selectN.setOnAction( new EventHandler<ActionEvent>() {
 
 				@Override
@@ -215,6 +243,7 @@ public class Main extends Application {
 				
 			});
 			
+			//EventHandler for closeButton Button to gracefully exit program.
 			closeButton.setOnAction( new EventHandler<ActionEvent>() {
 				
 				@Override
@@ -248,31 +277,36 @@ public class Main extends Application {
 
     public static Shape buildCardObject(String selection) {
         String selectedShape = selection.toLowerCase();
-
-        if(selectedShape == "rectangle") {
+        System.err.println( selectedShape );
+        if(selectedShape.equals("rectangle")) {
+        	System.out.println( "rectangle" );
             return new Rectangle();
-        } else if(selectedShape == "circle") {
+        } else if(selectedShape.equals("circle")) {
+        	System.out.println( "circle" );
             return new Circle();
-        } else if(selectedShape == "triangle") {
+        } else if(selectedShape.equals("triangle")) {
+        	System.out.println( "triangle" );
             Triangle temp = new Triangle();
             return temp.getTriangle();
-        } else if(selectedShape == "hexagon") {
+        } else if(selectedShape.equals("hexagon")) {
+        	System.out.println( "hexagon" );
             Hexagon temp = new Hexagon();
             return temp.getHexagon();
         }
+        System.out.println( "null" );
         return null; // you should never hit this
     }
 
     public static Color buildColorEnum(String selection) {
         String selectedColor = selection.toLowerCase();
 
-        if(selectedColor == "red") {
+        if(selectedColor.equals("red")) {
             return Color.RED;
-        } else if(selectedColor == "green") {
+        } else if(selectedColor.equals("green")) {
             return Color.GREEN;
-        } else if(selectedColor == "blue") {
+        } else if(selectedColor.equals("blue")) {
             return Color.BLUE;
-        } else if(selectedColor == "yellow") {
+        } else if(selectedColor.equals("yellow")) {
             return Color.YELLOW;
         }
         return null; //you should never hit this
